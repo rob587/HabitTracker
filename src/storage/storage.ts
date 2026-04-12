192a11525b67a54585a2a57589412123671bc781
rob587 <robertocamm02@yahoo.com>
2026-04-12 19:30:25 +0200

    function to update Habits

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -3,6 +3,7 @@ import { Habit } from "../types";
 
 const HABITS_KEY = "@habits";
 
+// TUTTE LE FUNZIONALITà DI HABITS
 export async function getHabits(): Promise<Habit[]> {
   try {
     const data = await AsyncStorage.getItem(HABITS_KEY);
@@ -18,4 +19,34 @@ export async function getHabits(): Promise<Habit[]> {
   }
 }
 
-export async function save(params: type) {}
+export async function saveHabit(habit: Habit): Promise<void> {
+  try {
+    const existingHabits = await getHabits();
+    const updatedHabits = [...existingHabits, habit];
+    await AsyncStorage.setItem(HABITS_KEY, JSON.stringify(updatedHabits));
+  } catch (error) {
+    console.error("Errore nel salvare Abitudine", error);
+  }
+}
+
+export async function updateHabit(
+  id: string,
+  updatedHabit: Habit,
+): Promise<void> {
+  try {
+    const existingHabits = await getHabits();
+    const index = existingHabits.findIndex((habit) => habit.id === id);
+
+    if (index === -1) {
+      console.warn(`Abitudine con id ${id} non trovata`);
+      return;
+    }
+
+    const updatedHabits = [...existingHabits];
+    updatedHabits[index] = updatedHabit;
+
+    await AsyncStorage.setItem(HABITS_KEY, JSON.stringify(updatedHabits));
+  } catch (error) {
+    console.error("Errore nell'aggiornare l'abitudine", error);
+  }
+}
