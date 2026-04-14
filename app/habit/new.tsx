3a3548a5f6721e44c71bb5939bb303cab4a422e0
rob587 <robertocamm02@yahoo.com>
2026-04-14 15:11:46 +0200

    Initial State control for input and standard values for colors and icons

--- app/habit/new.tsx
+++ app/habit/new.tsx
@@ -1,22 +1,46 @@
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function NewHabitScreen() {
+  // state iniziali per il form
+
+  const [name, setName] = useState("");
+  const [selectedColor, setSelectedColor] = useState("#6366F1");
+  const [selectedIcon, setSelectedIcon] = useState("🏃");
+  const [reminder, setReminder] = useState("");
+
+  // opzioni predefinite tra colori e icone
+  const colors = [
+    "#6366F1",
+    "#EF4444",
+    "#10B981",
+    "#F59E0B",
+    "#8B5CF6",
+    "#EC4899",
+    "#06B6D4",
+    "#84CC16",
+  ];
+
+  const icons = [
+    "🏃",
+    "📚",
+    "💪",
+    "🧘",
+    "🍎",
+    "💧",
+    "🛌",
+    "🎯",
+    "🧠",
+    "🎨",
+    "🎵",
+    "🧹",
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Aggiungi Abitudine</Text>
+    <View>
+      <Text>CIAO</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#121212",
-  },
-  text: {
-    color: "#ffffff",
-    fontSize: 20,
-  },
-});
+const styles = StyleSheet.create({});
